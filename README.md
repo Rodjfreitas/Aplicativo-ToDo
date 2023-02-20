16ac9c93500eec330bb4db711d8eccf932569e1e
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-02-20 18:10:37 -0300

    feat: iniciando projeto. cabeçalho, transição de tema, input ToDo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lista de tarefas ToDo
+
+Para este projeto, o objetivo é aprimorar os conhecimentos em html e css, com a utilização das boas práticas como tags semânticas em html, declaração de variáveis em css, e apresentar um código limpo e organizado.
+Outro objetivo é a evolução e desenvolvimento de habilidades em javascript. Estou me propondo a estudar funções em javascript e desenvolver o domínio sobre elas.
+
+
---
+++ js/ambiente.js
@@ -0,0 +1,27 @@
+const imagemsol = document.querySelector('.sol')
+const imagemlua = document.querySelector('.lua')
+
+imagemsol.addEventListener('click', function(){
+    document.body.classList.toggle('light')
+    
+})
+imagemlua.addEventListener('click', function(){
+    document.body.classList.toggle('light')
+    
+})
+
+
+function checkedInput(){
+    const checkbox = document.querySelector("#meuCheckbox")
+    const caixaSelecao = document.querySelector('.checkbox-wrapper')
+    const label = document.querySelector('label')
+    if(checkbox.checked == true){        
+        label.style.content = "\2713"
+        label.style.color = "white"
+        caixaSelecao.style.backgroundColor = "blue"
+    }else{
+        label.style.content = "\u2713"
+        label.style.color = "transparent"
+        caixaSelecao.style.backgroundColor = "transparent"
+    }
+}
