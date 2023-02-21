67f11f5cf9203a1fb764a9f43ecd01cff34efe55
Rodrigo Freitas <rodrigofreitas2011@live.com>
2023-02-20 22:04:05 -0300

    feat: adição de tarefas

--- js/ambiente.js
+++ js/ambiente.js
@@ -1,5 +1,8 @@
 const imagemsol = document.querySelector('.sol')
 const imagemlua = document.querySelector('.lua')
+const divs = document.querySelectorAll('.minhas-divs')
+const arrayDeDivs = []
+let x = 0
 
 imagemsol.addEventListener('click', function(){
     document.body.classList.toggle('light')
@@ -12,9 +15,9 @@ imagemlua.addEventListener('click', function(){
 
 
 function checkedInput(){
-    const checkbox = document.querySelector("#meuCheckbox")
+    const checkbox = document.querySelector(".meuCheckbox")
     const caixaSelecao = document.querySelector('.checkbox-wrapper')
-    const label = document.querySelector('.labelMain')
+    const label = document.querySelector('label')
     if(checkbox.checked == true){        
         label.style.content = "\2713"
         label.style.color = "var(--light-tema)"
@@ -25,3 +28,71 @@ function checkedInput(){
         caixaSelecao.style.backgroundColor = "transparent"
     }
 }
+
+
+
+
+function createDivs(){
+    const checkCaixa = document.querySelector('#meuCheckbox')
+    const inputText = document.querySelector('.inputTodo')    
+    const caixa = document.createElement('div')
+    caixa.setAttribute('class','minhas-divs')
+
+    if(checkCaixa.checked == true){
+        if(inputText.value != ""){
+
+            const chk = document.createElement('div')
+            chk.setAttribute('class','checkbox-wrapper')
+
+
+            
+
+            
+
+            const selecao = document.createElement('input')
+            selecao.setAttribute('id',`meuCheckbox-${x}`)
+            selecao.setAttribute('class','meuCheckbox')
+            selecao.setAttribute('type','checkbox')
+            selecao.setAttribute('onchange','checkedInput()')
+
+            
+
+            const textLabel = document.createElement('label')
+            textLabel.setAttribute('class',`labelMain`)
+            textLabel.setAttribute('for',`meuCheckbox-${x}`)
+            textLabel.innerText = "&#x2713"
+            
+            x++
+
+            const textoInput = document.createElement('p')
+            textoInput.setAttribute('class','inputTodo')
+            textoInput.innerText = inputText.value
+
+
+            chk.appendChild(textLabel)
+            chk.appendChild(selecao)
+            caixa.appendChild(chk)
+            caixa.appendChild(textoInput)
+
+            const secaoMain = document.querySelector('section')
+            secaoMain.appendChild(caixa)
+
+            checkCaixa.checked = false
+            inputText.value = ""
+
+            for(let i = 0; i < divs.length; i++){
+                arrayDeDivs.push(divs[i])
+            
+            }
+
+        }else{
+        alert('Adicione uma tarefa antes de clicar.')
+        checkCaixa.checked = false
+        }
+    }
+
+}
+
+
+
+console.log(arrayDeDivs)
